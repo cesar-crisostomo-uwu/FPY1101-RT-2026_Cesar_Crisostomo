f29bd7f83a5ce6fd36045caa66dc248b283da67e
Cesar Crisostomo <ce.crisostomo@duocuc.cl>
2026-07-13 00:05:41 -0400

    se valida opcion y se agregan librerias

--- prueba.py
+++ prueba.py
@@ -1,2 +1,30 @@
 
 
+def leer_opcion ():
+    while True:
+       print("  ========= GRIMORIO ASTRALIS =========")
+       print("1. Pergaminos por escuela de magia")
+       print ("2. Búsqueda de hechizos por rango de precio")
+       print("3. Actualizar precio de hechizo")
+       print("4. Agregar hechizo")
+       print("5. Eliminar hechizo")
+       print("6. Salir")
+       print("=====================================")
+
+       try:
+            opcion = int(input("elija una opcion 1-6:> "))
+            if 1<= opcion <= 6:
+                print("opcion valida")
+                return opcion
+            else:
+                print("elije un numero entre el 1 y el 6")
+                return 0
+       except ValueError:
+            print("ingrese un numero entero entre el 1 y el 6")
+            return 0 
+          
+  
+ 
+
+
+leer_opcion ()
